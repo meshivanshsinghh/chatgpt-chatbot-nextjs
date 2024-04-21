c500e1c50d6631abc3f326f521eb843aceff0422
Shivansh  Singh <shivanshsingh@Shivanshs-MacBook-Air.local>
2024-04-21 15:55:43 +0530

    created the closing and opening animation design and text field component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Chat from "@/components/Chat";
+import Chat from "@/app/components/Chat";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +19,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Chat />
-        {children}</body>
+        {children}
+      </body>
     </html>
   );
 }
